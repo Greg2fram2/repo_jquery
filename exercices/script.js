d4442ed94dadbcded395c5adda6ddb87473104a5
Greg2fram <greg2fram@gmail.com>
2020-10-26 11:54:29 +0100

    first commit, 3 files

---
+++ exercices/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("p").hide();     //Pour cacher le nom "p" (id ou classe)
+    $("#test").hide(); //Pour cacher les id = test
+    $(".test").hide(); //Pour cacher les classes = test
+    //$("p").hide();   //Pour cacher tous les éléments
+})
